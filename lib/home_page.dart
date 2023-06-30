58f02d55c510f206488073a95ddc464cf205d4d8
Antonio-Secchin <101224741+Antonio-Secchin@users.noreply.github.com>
2023-06-30 09:08:48 -0300

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,15 +6,19 @@ class HomePage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
+    //Salva o Home Controller na variavel controller
     var controller = context.dependOnInheritedWidgetOfExactType<HomeController>();
 
     return Scaffold(
       appBar: AppBar(
+        //Define o titulo do aplicativo
         title: const Text('Home'),
       ),
       body:  Center(
+        //Define um texto para aparecer no centro da tela do aplicativo
         child: Text('Primeiro Codigo: ${controller?.value}'),
       ),
+      //Cria um botão que pode ser clicado para gerar um hello world no console de depuração
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: (){
